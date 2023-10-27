f9cf9c35a959c98c887a292664368591d2274409
SeYeD <mohamad.khoshnava@gmail.com>
2023-10-27 16:44:29 +0330

    up version to 0.1.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="aggify",
-    version="0.1.1",
+    version="0.1.2",
     description="A MongoDB aggregation generator for Mongoengine",
     author="SeYeD.Dev",
     author_email="me@seyed.dev",
