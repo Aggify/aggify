285a1ea4aac1913539083033a1eeb09eda30ce7d
SeYeD <mohamad.khoshnava@gmail.com>
2023-10-27 19:03:49 +0330

    fix readme

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="aggify",
-    version="0.1.4",
+    version="0.1.5",
     description="A MongoDB aggregation generator for Mongoengine",
     author="SeYeD.Dev",
     author_email="me@seyed.dev",
