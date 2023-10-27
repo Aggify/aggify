97b77d89bb66f77d8594e8a97d74e6bef925fb62
MahdiHaghverdi <mahdihaghverdliewpl@gmail.com>
2023-10-27 18:05:18 +0330

    Remove `to_dict` and make `Q` object support the `dict` constructor

--- tests/test_q.py
+++ tests/test_q.py
@@ -8,29 +8,29 @@ class TestQ:
         q1 = Q(name="John")
         q2 = Q(name="Alice")
         q_combined = q1 | q2
-        assert q_combined.to_dict() == {
-            "$match": {"$or": [q1.to_dict()["$match"], q2.to_dict()["$match"]]}}
+        assert dict(q_combined) == {
+            "$match": {"$or": [dict(q1)["$match"], dict(q2)["$match"]]}}
 
     def test_or_operator_with_multiple_conditions_more_than_rwo(self):
         q1 = Q(name="John")
         q2 = Q(name="Alice")
         q3 = Q(name="Bob")
         q_combined = q1 | q2 | q3
-        assert q_combined.to_dict() == {
-            "$match": {"$or": [q1.to_dict()["$match"], q2.to_dict()["$match"], q3.to_dict()["$match"]]}}
+        assert dict(q_combined) == {
+            "$match": {"$or": [dict(q1)["$match"], dict(q2)["$match"], dict(q3)["$match"]]}}
 
     # Test combining NOT operators with AND
     def test_combine_not_operators_with_and(self):
         q1 = Q(name="John")
         q2 = Q(age__lt=30)
         q_combined = ~q1 & ~q2
-        assert q_combined.to_dict() == {
-            "$match": {"$and": [{"$not": [q1.to_dict()["$match"]]}, {"$not": [q2.to_dict()["$match"]]}]}}
+        assert dict(q_combined) == {
+            "$match": {"$and": [{"$not": [dict(q1)["$match"]]}, {"$not": [dict(q2)["$match"]]}]}}
 
     # Test combining NOT operators with OR
     def test_combine_not_operators_with_or(self):
         q1 = Q(name="John")
         q2 = Q(age__lt=30)
         q_combined = ~q1 | ~q2  # Changed | to combine OR
-        assert q_combined.to_dict() == {
-            "$match": {"$or": [{"$not": [q1.to_dict()["$match"]]}, {"$not": [q2.to_dict()["$match"]]}]}}
+        assert dict(q_combined) == {
+            "$match": {"$or": [{"$not": [dict(q1)["$match"]]}, {"$not": [dict(q2)["$match"]]}]}}
