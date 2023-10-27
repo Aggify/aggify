55f46cf7227415c8857e6ac4f7ca108099972c95
SeYeD <mohamad.khoshnava@gmail.com>
2023-10-27 15:14:01 +0330

    change repo url

--- setup.py
+++ setup.py
@@ -2,21 +2,19 @@ from setuptools import setup, find_packages
 
 setup(
     name="aggify",
-    version="0.1.0",
+    version="0.1.1",
     description="A MongoDB aggregation generator for Mongoengine",
     author="SeYeD.Dev",
     author_email="me@seyed.dev",
-    url="https://github.com/seyed-dev/aggify",
+    url="https://github.com/Aggify/aggify",
     packages=find_packages(),
     install_requires=["mongoengine >= 0.27.0"],
-    python_requires=">=3.9",
+    python_requires=">=3.10",
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
     ],
 )
