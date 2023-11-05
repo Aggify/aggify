f602e9e7ee5f57d3d7dfe6916608eb8d524b4f0a
SeYeD <mohamad.khoshnava@gmail.com>
2023-11-06 00:50:18 +0330

    fix import

--- aggify/types.py
+++ aggify/types.py
@@ -1,5 +1,6 @@
 from typing import Union, Dict, TypeVar, Callable
 from bson import ObjectId
+from mongoengine import Document
 
 QueryParams = Union[int, None, str, bool, float, Dict, ObjectId]
-CollectionType = TypeVar("CollectionType", bound=Callable[..., "Document"])
+CollectionType = TypeVar("CollectionType", bound=Callable[..., Document])
